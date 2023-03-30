3c0e38865fad4f3cc3946fbe2f89689142b2b970
Regina Lubkovskaia <118375663+Reginababaika@users.noreply.github.com>
2023-03-30 18:04:19 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,14 +1,38 @@
 from rest_framework import permissions
 
 
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user.role == 'admin' and
+            request.user.is_authenticated or
+            request.user.is_superuser
+        )
+
 class IsAdmin(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_admin and request.user.is_authenticated
+    def has_permission(self, request, view):
+        return bool(
+            request.user.role == 'admin' and
+            request.user.is_authenticated or
+            request.user.is_superuser
+        )
 
+class IsUserOrReadOnly(permissions.BasePermission):
 
-class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user.is_authenticated
+        )
 
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_moderator and request.user.is_authenticated
+class IsModeratorOrReadOnly(permissions.BasePermission):
 
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user.role == 'moderator' and
+            request.user.is_authenticated
+        )
