4040cbffcdd4d767857fbc163253b31bdc82658a
Regina Lubkovskaia <118375663+Reginababaika@users.noreply.github.com>
2023-03-31 10:52:35 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,8 +6,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.method in permissions.SAFE_METHODS or
-            request.user.role == 'admin' and
-            request.user.is_authenticated or
+            request.user.is_authenticated and
+            request.user.role == 'admin' or
             request.user.is_superuser
         )
 
@@ -15,8 +15,8 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            request.user.role == 'admin' and
-            request.user.is_authenticated or
+            request.user.is_authenticated and
+            request.user.role == 'admin' or
             request.user.is_superuser
         )
 
@@ -33,6 +33,8 @@ class IsModeratorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.method in permissions.SAFE_METHODS or
-            request.user.role == 'moderator' and
-            request.user.is_authenticated
+            request.user.is_authenticated and
+            request.user.role == 'moderator' or
+            request.user.role == 'admin' or
+            request.user.is_superuser
         )
